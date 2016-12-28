c06f1b8bcedecafdea819277b9685e94f1ae2f31
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-12-28 20:18:48 -0200

    Execution througth config.ini

--- src/os_utils.py
+++ src/os_utils.py
@@ -1,9 +1,26 @@
 import os
 
-path_reference = "/home/faccioli/Execute/teste_webService/virtual_screening"
 def run_command(command=None):
-    command_exec = "cd "
-    command_exec += path_reference
-    command_exec += " && "
-    command_exec += command
-    os.system(command_exec)
+    print command    
+    if command == None:
+        raise("command is empty")
+    os.system(command)
+
+def get_spark_command(dic_param):
+    path_file = os.path.join(dic_param['path_spark_drugdesign'], dic_param['spark_file'])
+    command = dic_param['spark_submit']
+    command += " "
+    command += path_file
+    return command
+
+def get_command_chdir(dic_param):
+    command = "cd "
+    command += dic_param['path_execution']
+    command += " "
+    return command
+
+def join_2_commands_to_run(command_1, command_2):
+    command = command_1
+    command += " && "
+    command += command_2
+    return command
--- src/vs_app.py
+++ src/vs_app.py
@@ -1,13 +1,24 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required, current_identity
-from os_utils import run_command
+from os_utils import run_command, get_spark_command, get_command_chdir, join_2_commands_to_run
+
+
 
 #from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'mi4u_vs'
 api = Api(app)
+
+import ConfigParser as configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+dic_param = {}
+dic_param['path_spark_drugdesign'] = config.get('DRUGDESIGN', 'path_spark_drugdesign')
+dic_param['path_execution'] = config.get('DRUGDESIGN_API', 'path_execution')
+dic_param['spark_submit'] = config.get('DRUGDESIGN_API', 'spark_submit')
+
 
 class PrepareLibrary(Resource):
     parser = reqparse.RequestParser()
@@ -19,12 +30,16 @@ class PrepareLibrary(Resource):
 
 #    @jwt_required()
     def post(self, name):
-
         data = PrepareLibrary.parser.parse_args()
-        command = "/home/faccioli/Programs/spark-1.4.1-bin-hadoop2.4/bin/spark-submit /home/faccioli/workspace/drugdesign/virtualscreening/vina/spark/prepare_ligand.py "
-        print(command)
+#        print data['name']
+        dic_param['spark_file'] = "prepare_ligand.py"
+
+        chdir = get_command_chdir(dic_param)
+        spark_command = get_spark_command(dic_param)
+        command = join_2_commands_to_run(chdir, spark_command)
         run_command(command)
-        library = {'name': name}
+
+        library = {'name': command}
         return library
 
 api.add_resource(PrepareLibrary, '/preparelibrary/<string:name>')
