9f0c020ca1cbe4fbb7aa90601ba9165735fc0003
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-12-28 22:52:55 -0200

    Added ligand repository

--- src/vs_app.py
+++ src/vs_app.py
@@ -19,29 +19,34 @@ dic_param['path_spark_drugdesign'] = config.get('DRUGDESIGN', 'path_spark_drugde
 dic_param['path_execution'] = config.get('DRUGDESIGN_API', 'path_execution')
 dic_param['spark_submit'] = config.get('DRUGDESIGN_API', 'spark_submit')
 dic_param['receptor_repository'] = config.get('DRUGDESIGN_API', 'receptor_repository')
+dic_param['ligand_repository'] = config.get('DRUGDESIGN_API', 'ligand_repository')
 
 
 class PrepareLibrary(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name',
+    parser.add_argument('ligandlib',
         type=str,
         required=True,
         help="This field cannot be left blank!"
     )
 
 #    @jwt_required()
-    def post(self, name):
+    def post(self, ligandlib):
         data = PrepareLibrary.parser.parse_args()
-#        print data['name']
         dic_param['spark_file'] = "prepare_ligand.py"
+
+        dir_ligand_repository_param = join_directory(dic_param['ligand_repository'] ,data['ligandlib'])
+        if check_diretory_exists(dir_ligand_repository_param) == False:
+            mens_ret = "Ligand library needs to be uploaded"
 
         chdir = get_command_chdir(dic_param)
         spark_command = get_spark_command(dic_param)
         command = join_2_commands_to_run(chdir, spark_command)
         run_command(command)
+        mens_ret = "Prepare ligand was executed successfuly"
 
-        library = {'name': command}
-        return library
+        mens_ret = {'lig_men': mens_ret}
+        return mens_ret
 
 class PrepareReceptor(Resource):
     parser = reqparse.RequestParser()
@@ -69,7 +74,7 @@ class PrepareReceptor(Resource):
         mens_ret = {'men_receptor': mens_ret}
         return mens_ret
 
-api.add_resource(PrepareLibrary, '/preparelibrary/<string:name>')
+api.add_resource(PrepareLibrary, '/preparelibrary/<string:ligandlib>')
 api.add_resource(PrepareReceptor, '/preparereceptor/<string:receptor>')
 
 if __name__ == '__main__':
